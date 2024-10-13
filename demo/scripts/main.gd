6fe9785be2d9f44bcb0b55f6651eff27babb8d59
JayMottershead <jay-mottershead@hotmail.com>
2024-10-13 13:01:58 +0100

    Update: Tidying Up
    Needed to get a clean commit locally to try and workout how to debug GDExtension code. Kinda messed up my local version so had to remove the repo and redownload it to start again.

--- demo/scripts/main.gd
+++ demo/scripts/main.gd
@@ -1,4 +1,4 @@
 extends Node
 
-func _on_sprite_position_changed(node: Object, new_pos: Vector2) -> void:
+func _on_demo_example_position_changed(node: Object, new_pos: Vector2) -> void:
 	print("The position of " + node.get_class() + " is now " + str(new_pos))
